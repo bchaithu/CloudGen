eddc0d092b5cdac2fedf494e51c655fbc2266ca2
chaitanya <dimpuchaithu@gmail.com>
2022-02-16 19:33:43 +0530

    First Commit(Chaitanya)

---
+++ CloudGen/src/main/java/com/cloudgen/webapp/CloudGenApplication.java
@@ -0,0 +1,13 @@
+package com.cloudgen.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGenApplication.class, args);
+	}
+
+}
